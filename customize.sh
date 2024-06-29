c5094e372b2ef749893c2f984578c3572c47a669
Your Name <you@example.com>
2024-06-29 21:28:14 +0800

    Update

--- customize.sh
+++ customize.sh
@@ -6,69 +6,72 @@
 # is installing in recovery mode?
 $BOOTMODE || abort "Please install in Magisk App!"
 
-# Exit on error
-#set -e
-
 BPATH="$MODPATH/system/xbin"
+MODVER="$(grep_prop version ${TMPDIR}/module.prop)"
 
-deploy(){
-# Init
-chmod 755 $BPATH/*
+deploy() {
+	# Init
+	chmod 755 $BPATH/*
 
-# Detect Architecture
+	# Detect Architecture
 
-# 64 Bit
-if [ "$ARCH" = "arm64" ]; then
-mv -f $BPATH/busybox-arm64 $BPATH/busybox
-rm -f $BPATH/busybox-arm
-ui_print "- $ARCH arch detected."
-fi
+	# 64 Bit
+	if [ "$ARCH" = "arm64" ]; then
+		mv -f $BPATH/busybox-arm64 $BPATH/busybox
+		rm -f $BPATH/busybox-arm
+		ui_print "- $ARCH arch detected."
+	fi
 
-# 32 Bit
-if [ "$ARCH" = "arm" ]; then
-mv -f $BPATH/busybox-arm $BPATH/busybox
-rm -f $BPATH/busybox-arm64
-ui_print "- $ARCH arch detected."
-fi
+	# 32 Bit
+	if [ "$ARCH" = "arm" ]; then
+		mv -f $BPATH/busybox-arm $BPATH/busybox
+		rm -f $BPATH/busybox-arm64
+		ui_print "- $ARCH arch detected."
+	fi
 
-# Not Supported for x86 and x64 ARCH
-if [ "$ARCH" = "x86" ]; then
-rm -rf $MODPATH
-abort "- $ARCH arch not supported."
-fi
+	# Not Supported for x86 and x64 ARCH
+	if [ "$ARCH" = "x86" ]; then
+		rm -rf $MODPATH
+		abort "- $ARCH arch not supported."
+	fi
 
-if [ "$ARCH" = "x64" ]; then
-rm -rf $MODPATH
-abort "- $ARCH arch not supported."
-fi
-
+	if [ "$ARCH" = "x64" ]; then
+		rm -rf $MODPATH
+		abort "- $ARCH arch not supported."
+	fi
 }
+
+ui_print "- Module Version $MODVER"
 
 # Check for another busybox
 if ! [ -d "/data/adb/modules/eraselk_busybox" ]; then
-if [ -e /system/xbin/busybox ]; then
-rm -rf $MODPATH
-abort "- Please uninstall another busybox from /system/xbin/ and reboot."
-fi
-if [ -e /system/bin/busybox ]; then
-rm -rf $MODPATH
-abort "- Please uninstall another busybox from /system/bin/ and reboot."
-fi
-if [ -e /vendor/bin/busybox ]; then
-rm -rf $MODPATH
-abort "- Please uninstall another busybox from /vendor/bin/ and reboot."
-fi
+	if [ -e /system/xbin/busybox ]; then
+		rm -rf $MODPATH
+		abort "- Please uninstall another busybox from /system/xbin/ and reboot."
+	fi
+	if [ -e /system/bin/busybox ]; then
+		rm -rf $MODPATH
+		abort "- Please uninstall another busybox from /system/bin/ and reboot."
+	fi
+	if [ -e /vendor/bin/busybox ]; then
+		rm -rf $MODPATH
+		abort "- Please uninstall another busybox from /vendor/bin/ and reboot."
+	fi
 fi
 
 # Extract Binary
 deploy
 
+# Print Busybox Version
+BB_VER=$($BPATH/busybox | head -n1 | cut -f1 -d'(' | awk '{print $2}')
+ui_print "- Busybox Version $BB_VER"
+
 # Install into /system/bin, if exists.
 if [ ! -e /system/xbin ]; then
-mkdir -p $MODPATH/system/bin
-mv -f $BPATH/busybox $MODPATH/system/bin/busybox
-rm -Rf $BPATH
-ui_print "- Installing into /system/bin.."
+	mkdir -p $MODPATH/system/bin
+	mv -f $BPATH/busybox $MODPATH/system/bin/busybox
+	rm -Rf $BPATH
+	ui_print "- Installing into /system/bin.."
 fi
 
 # Print Success
--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,17 +5,18 @@
 
 MODDIR=${0%/*}
 
- if [ ! -d $MODDIR/system/xbin ]; then
- chown 0:0 $MODDIR/system/bin/busybox;
- chmod 775 $MODDIR/system/bin/busybox;
- chcon u:object_r:system_file:s0 $MODDIR/system/bin/busybox;
- $MODDIR/system/bin/busybox --install -s $MODDIR/system/bin/;
- for sd in /system/bin/*; do
-   rm -f $MODDIR/${sd};
- done;
-else
- chown 0:0 $MODDIR/system/xbin/busybox;
- chmod 775 $MODDIR/system/xbin/busybox;
- chcon u:object_r:system_file:s0 $MODDIR/system/xbin/busybox;
- $MODDIR/system/xbin/busybox --install -s $MODDIR/system/xbin/;
- fi;
+if ! [ -f "$MODDIR/installed" ]; then
+	if [ -d "$MODDIR/system/bin" ]; then
+		chown 0:0 $MODDIR/system/bin/busybox
+		chmod 775 $MODDIR/system/bin/busybox
+		chcon u:object_r:system_file:s0 $MODDIR/system/bin/busybox
+		$MODDIR/system/bin/busybox --install -s $MODDIR/system/bin/
+	fi
+	if [ -d "$MODDIR/system/xbin" ]; then
+		chown 0:0 $MODDIR/system/xbin/busybox
+		chmod 775 $MODDIR/system/xbin/busybox
+		chcon u:object_r:system_file:s0 $MODDIR/system/xbin/busybox
+		$MODDIR/system/xbin/busybox --install -s $MODDIR/system/xbin/
+	fi
+	touch $MODDIR/installed
+fi
